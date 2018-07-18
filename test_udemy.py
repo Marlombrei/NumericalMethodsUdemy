90b401a9489e3a1f95d0c39503e118568e45c3e4
Marlombrei <marlombrei@users.noreply.github.com>
2018-07-18 23:01:41 +0100

    Second file

---
+++ test_udemy.py
@@ -0,0 +1,46 @@
+from __future__ import division
+import numpy as np
+import scipy as sp
+import Integration
+from scipy import integrate
+
+
+x = np.arange(6)
+y = np.array([2,8,14,28,39,62])
+
+Integration.Polynomial_fitting(x,y,n=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
